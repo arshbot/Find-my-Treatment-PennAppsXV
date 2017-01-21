0d52f1498d9d7029c66c90b7b073b52fbaf3d244
HarshaGoli <harshagoli@gmail.com>
2017-01-21 16:57:11 -0500

    Added Textview and centered

--- Find My Treatment/Find My Treatment/ViewController.swift
+++ Find My Treatment/Find My Treatment/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+     // You don't need to modify the default init(nibName:bundle:) method.
+    
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          setupTextView()
+     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+     }
+    
+     func setupTextView(){
+          let textField: UITextField = UITextField(frame: CGRect(x: 0, y: 0, width: 300, height: 40.00));
+          self.view.addSubview(textField)
+     
+          textField.center = CGPoint(x: UIScreen.main.bounds.midX, y: 100)
+          textField.backgroundColor = UIColor.white
+          textField.text = "What need's treatin'?"
+          textField.borderStyle = UITextBorderStyle.roundedRect
+     
+     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+     
+
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
 
 
 }
