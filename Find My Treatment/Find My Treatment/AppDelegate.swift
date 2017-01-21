3bc6a65ae85c172ad4693b9ab52e76179ecfed9a
HarshaGoli <harshagoli@gmail.com>
2017-01-21 16:56:17 -0500

    Added API keys

--- Find My Treatment/Find My Treatment/AppDelegate.swift
+++ Find My Treatment/Find My Treatment/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyAK-GsGgmWQz2eQ_OHhpGLWZUlSi5CZG24")
+        GMSPlacesClient.provideAPIKey("AIzaSyAK-GsGgmWQz2eQ_OHhpGLWZUlSi5CZG24")
         return true
     }
 
